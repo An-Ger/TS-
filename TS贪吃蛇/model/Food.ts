c1e930459a6fd6bf802459aa17024227d2123c45
an-ger <aiboowen@gmail.com>
2021-01-23 15:43:17 +0800

    食物类

---
+++ TS贪吃蛇/model/Food.ts
@@ -0,0 +1,20 @@
+class Food {
+    element: HTMLElement;
+    constructor() {
+      this.element = document.getElementById("food")!; //!表示不会为空
+    }
+    get X() {
+      return this.element.offsetLeft;
+    }
+    get Y() {
+      return this.element.offsetTop;
+    }
+    change() {
+      //位置最小是0，最大是290，必须是整⑩
+      let left = Math.round(Math.random() * 29) * 10;
+      let top = Math.round(Math.random() * 29) * 10;
+      this.element.style.left = left + "px";
+      this.element.style.top = top + "px";
+    }
+  }
+  export default Food;

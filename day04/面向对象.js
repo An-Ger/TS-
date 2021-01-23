32c91d3277f8514ec395b8a82e7a573183dbe395
an-ger <aiboowen@gmail.com>
2021-01-23 12:33:19 +0800

    背景布局

--- day04/面向对象.js
+++ day04/面向对象.js
@@ -3,7 +3,7 @@ var Person = /** @class */ (function () {
     }
     Person.prototype.sayHello = function () {
         console.log("hello");
-    };
+    }; //方法以static开头则是静态方法，否则需要通过实例调用
     //属性前使用static可以定义静态属性，静态属性不需要创建类就可以使用
     Person.age = 19;
     return Person;
